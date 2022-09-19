3064051a4c79f3f81e6f5f1698780a1400cbbaaf
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2022-09-19 12:21:52 +0600

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -188,42 +188,42 @@ const $ = (
     } else _query(node, props);
 };
 
-// Box Component. similar to `<div></div>`
+// Box Component. Similar to `<div></div>`
 class Box extends BaseComponent {
     constructor(props: ComponentProps) {
         super("div", props);
     }
 }
 
-// Field Component. similar to `<span></span>`
+// Field Component. Similar to `<span></span>`
 class Field extends BaseComponent {
     constructor(props: ComponentProps) {
         super("span", props);
     }
 }
 
-// Title Component. similar to `<h1></h1>`
+// Title Component. Similar to `<h1></h1>`
 class Title extends BaseComponent {
     constructor(props: ComponentProps) {
         super("h1", props);
     }
 }
 
-// Subtitle Component. similar to `<h2></h2>`
+// Subtitle Component. Similar to `<h2></h2>`
 class Subtitle extends BaseComponent {
     constructor(props: ComponentProps) {
         super("h2", props);
     }
 }
 
-// Paragraph Component. similar to `<p></p>`
+// Paragraph Component. Similar to `<p></p>`
 class Paragraph extends BaseComponent {
     constructor(props: ComponentProps) {
         super("p", props);
     }
 }
 
-// Button Component. similar to `<button></button>`
+// Button Component. Similar to `<button></button>`
 class Button extends BaseComponent {
     constructor(props: ComponentProps) {
         super("button", props);
